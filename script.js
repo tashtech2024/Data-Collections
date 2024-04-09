44f02861d727d3fcddd189742fd29d174bd64624
Latasha Davis <latashaddavis21@gmail.com>
2024-04-09 17:39:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+console.log(`Running......`);
+
+// Arrays Defination 
+
+const names = ["Tori", "Jen", "Mari"];
+
+console.log(names);
+console.log(names.length);
+
+// console.log(names[0]);
+// console.log([names.length 0]);
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+const movies = ['Easy A', 'Bring iT oN', 'Batman']
+
+console.log(movies); {
+
+    let firstMovie = movies[0];
+
+    console.log(firstMovie);
+}
+movies[3] = 'Spongbob';
+console.log(movies);
+
+movies[4] = 'Popcorn';
+console.log(movies);
+
+movies [movies.length] = 'Fast 5';
+
+console.log(movies);
+
+// 
+let firstMovie = movies[0];
+console.log(firstMovie);
+
+movies[10] = 'Shrek';
+
+movies.length = 50;
+console.log(movies);
+
+//  FOR LOOP IN AN ARRAY FORM!!!!!>
+for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i]);
+}
+
+const newMovies = ['Caddyshack','Two can play that game', 'Scarface'];
+
+console.log(movies);
+
+movies[0];
+
+//     //  REDO BELOW === ///
+// }
+// const myColors = ['red', 'blue', 'green'];
+// const yourColors = ['yellow', 'purple', 'pink'];
+
+// let theColors = yourColors.concat(myColors);
+
+// console.log(myColors);
+// console.log(yourColors);
+
+// const ourColors = [];
+// console.log(ourColors);
+
+// // for (let color of myColors){
+// //     ourColors.push{color};
+// // }
+// //     for (let color of yourColors){
+// //         ourColors.push{color};
+
+// // }
+// // console.log(ourColors);
+
+// // ========
+// // const myColorscopy = mycolors.concat [];
+// myColors.push('gray')
+
+// console.log(myColors);
+// console.log(myColors);
+
+// const allColorStrings = ourColors.joint(',') 
+// console.log("I like ${allColorStrings}");
+
+// // remove from the end of the array //
+
+// movies.pop [];
+
+//
+// let firstMovie = movies [0]
+// console.log(firstMovie)
